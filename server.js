7abdb1e0fe9036833225ba3d7c39591b7f53032a
arjunrops <55598748+arjunrops@users.noreply.github.com>
2020-12-02 08:34:31 +0530

    adding new files

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'realmadridcr7',
+        database: 'smart_brain'
+    }
+});
+
+const app = express();
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Arjun',
+            email: 'arjun@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Ajax',
+            email: 'ajax@gmail.com',
+            password: 'johan',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+    console.log('app is running in port 3000');
+})
